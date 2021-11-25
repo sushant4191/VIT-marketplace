9bb6d2d0b8ef8978efe69c54cab3378976124f22
sushant4191 <metalsushant@gmail.com>
2021-11-25 09:33:52 +0545

    footer-design

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,70 @@
+import { Facebook, Instagram, LinkedIn, Twitter } from '@material-ui/icons';
 import React from 'react'
+import { Box,Container,Row,Column,FooterLink,Heading, FooterItem, Icon} from "./FooterStyles";
 
 const Footer = () => {
     return (
-        <div>
-            Footer
-        </div>
-    )
+        <Box>
+        <h1 style={{ color: "white", 
+                     textAlign: "center", 
+                     marginTop: "-50px",
+                     marginBottom:"10px"}}>
+          MarVIT
+        </h1>
+        <Container>
+          <Row>
+            <Column>
+              <Heading>About Us</Heading>
+              <FooterLink href="#">Aim</FooterLink>
+              <FooterLink href="#">Vision</FooterLink>
+              <FooterLink href="#">Testimonials</FooterLink>
+            </Column>
+            <Column>
+              <Heading>Services</Heading>
+              <FooterLink href="#">Writing</FooterLink>
+              <FooterLink href="#">Internships</FooterLink>
+              <FooterLink href="#">Coding</FooterLink>
+              <FooterLink href="#">Teaching</FooterLink>
+            </Column>
+            <Column>
+              <Heading>Contact Us</Heading>
+              <FooterLink href="#">Uttar Pradesh</FooterLink>
+              <FooterLink href="#">Ahemdabad</FooterLink>
+              <FooterLink href="#">Indore</FooterLink>
+              <FooterLink href="#">Mumbai</FooterLink>
+            </Column>
+            <Column>
+                <Heading>Sell Here</Heading>
+            </Column>
+          </Row>
+          
+        </Container>
+        <FooterItem>
+              <Icon href="#">                             
+                  <Facebook/>            
+              </Icon>
+              <Icon href="#">  
+                  <Twitter/>         
+              </Icon>
+              <Icon href="#">  
+                  <Instagram/>            
+              </Icon>
+              <Icon href="#">  
+                  <LinkedIn/>
+              </Icon>
+             </FooterItem>
+             
+          <h4 style={{ color: "white", 
+                     textAlign: "center", 
+                     marginTop: "12px",
+                     marginBottom:"-3em"}}>
+          Made with 💗 by Sushant
+        </h4>
+      </Box>
+      
+      
+    );
 }
 
 export default Footer
+
---
+++ src/components/FooterStyles.js
@@ -0,0 +1,96 @@
+import styled from 'styled-components';
+   
+export const Box = styled.div`
+  padding: 80px 60px;
+  background: #61626d; 
+  position: relative;
+  bottom: 0;
+  /* right:0; */
+  width: 100%;
+
+  @media (max-width: 1000px) {
+    padding: 70px 30px;
+  }
+`;
+   
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    max-width: 1000px;
+    margin: 0 auto;
+    /* background: red; */
+    letter-spacing: 1px;
+`
+   
+export const Column = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: left;
+  margin-left: 60px;
+`;
+   
+export const Row = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, 
+                         minmax(185px, 1fr));
+  grid-gap: 20px;
+   
+  @media (max-width: 1000px) {
+    grid-template-columns: repeat(auto-fill, 
+                           minmax(200px, 1fr));
+  }
+`;
+   
+export const Heading = styled.p`
+  font-size: 24px;
+  color: #fff;
+  margin-bottom: 40px;
+  font-weight: bold;
+`;
+
+export const FooterLink = styled.a`
+  color: #fff;
+  margin-bottom: 10px;
+  font-size: 18px;
+  text-decoration: none;
+   
+  &:hover {
+      color: #2c2c2c;
+      transition: 200ms ease-in;
+  }
+`;
+
+export const FooterItem = styled.div`
+  color: #fff;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  margin: 20px auto;
+  align-items: center;
+  margin-bottom: -5px;
+
+  &:hover {
+      color: #2c2c2c;
+      transition: 200ms ease-in;
+  }
+`;
+
+export const Icon = styled.a`
+    width:40px;
+    height: 40px;
+    border-radius:50%;
+    background-color: white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 10px 20px;
+    transition: all 0.25s ease;
+
+    &:hover{
+        background-color:#e9f5f5;
+        transform:scale(1.1);
+    }
+`;
+   
+   
