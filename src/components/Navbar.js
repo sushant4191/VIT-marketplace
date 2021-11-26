09b20c5817bc5f4ab86913487e7b980f5209908d
sushant4191 <metalsushant@gmail.com>
2021-11-26 16:01:26 +0545

    nav bar button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -33,20 +33,23 @@ function NavBar() {
                 <Nav.Link><Link to="/allproducts">All Products</Link></Nav.Link>
                 <Nav.Link><Link to="/contactus">Contact Us</Link></Nav.Link>
                 <Nav.Link><Link to="/profile">Profile</Link></Nav.Link>
-               <Nav.Link><Link to="/"><button onClick={() => {Cookies.remove('token'); window.location.reload();}} class="py-1 px-4 flex justify-center items-center  bg-red-600 hover:bg-red-700 focus:ring-red-500 focus:ring-offset-red-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">Logout</button></Link></Nav.Link>
+               {/* <Nav.Link><Link to="/"><button onClick={() => {Cookies.remove('token'); window.location.reload();}} class="py-1 px-4 flex justify-center items-center  bg-red-600 hover:bg-red-700 focus:ring-red-500 focus:ring-offset-red-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">Logout</button></Link></Nav.Link> */}
             </Nav>
-                <Form className="d-flex">
+                {/* <Form className="d-flex">
                     <FormControl
                     type="search"
                     placeholder="Search"
                     className="me-2"
                     aria-label="Search"/>
                 <Button variant="outline-success">Search</Button>
-            </Form>
+            </Form> */}
             <MenuItem>
-            <Badge badgeContent="4" color="primary">
+            <Nav.Link><Link to="/"><button onClick={() => {Cookies.remove('token'); window.location.reload();}} class="py-1 px-4 flex justify-center items-center  bg-red-600 hover:bg-red-700 focus:ring-red-500 focus:ring-offset-red-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">Logout</button></Link></Nav.Link>
+
+
+            {/* <Badge badgeContent="4" color="primary">
                 <ShoppingCartOutlined/>
-            </Badge>
+            </Badge> */}
             </MenuItem>
             </Navbar.Collapse>
             
